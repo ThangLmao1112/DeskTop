c6b651c813e81ac30bf1f1b6edce7e773cf568d6
ThangLmao1112 <147223797+ThangLmao1112@users.noreply.github.com>
2024-11-05 08:23:38 +0700

    Luu

---
+++ Lap07/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            string connectionString = "server=PC827; database= RestaurantManagement; Integrated Security = true; ";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ID, Name FROM Category";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            // Mở kết nối
+            conn.Open();
+
+            // Lấy dữ liệu từ cơ sở dữ liệu đưa vào DataTable
+            adapter.Fill(dt);
+
+            // Đóng kết nối và giải phóng bộ nhớ
+            conn.Close();
+            conn.Dispose();
+
+            // Đưa dữ liệu vào ComboBox
+            cbbCategory.DataSource = dt;
+
+            // Hiển thị tên nhóm sản phẩm
+            cbbCategory.DisplayMember = "Name";
+
+            // Nhưng khi lấy giá trị thì lấy ID của nhóm
+            cbbCategory.ValueMember = "ID";
+        }
+    }
+}
