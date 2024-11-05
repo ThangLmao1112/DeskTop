8c4045ddba8d2b6a38cfe6f6992cbf39ac80f5c2
ThangLmao1112 <147223797+ThangLmao1112@users.noreply.github.com>
2024-11-05 10:57:13 +0700

    Luu

---
+++ Lab07/Lab07/fOODInfoForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Lab07
+{
+    partial class fOODInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFoodID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.cbbCatName = new System.Windows.Forms.ComboBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.nudPrice = new System.Windows.Forms.NumericUpDown();
+            this.btnAddFood = new System.Windows.Forms.Button();
+            this.btnUpdateFood = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FoodID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Unit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Category Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 244);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Notes";
+            // 
+            // txtFoodID
+            // 
+            this.txtFoodID.Location = new System.Drawing.Point(163, 32);
+            this.txtFoodID.Name = "txtFoodID";
+            this.txtFoodID.ReadOnly = true;
+            this.txtFoodID.Size = new System.Drawing.Size(127, 20);
+            this.txtFoodID.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(163, 69);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(310, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Location = new System.Drawing.Point(163, 108);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(310, 20);
+            this.txtUnit.TabIndex = 8;
+            // 
+            // cbbCatName
+            // 
+            this.cbbCatName.FormattingEnabled = true;
+            this.cbbCatName.Location = new System.Drawing.Point(163, 154);
+            this.cbbCatName.Name = "cbbCatName";
+            this.cbbCatName.Size = new System.Drawing.Size(219, 21);
+            this.cbbCatName.TabIndex = 9;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(398, 154);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 10;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(163, 241);
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(310, 20);
+            this.txtNotes.TabIndex = 11;
+            // 
+            // nudPrice
+            // 
+            this.nudPrice.Location = new System.Drawing.Point(163, 200);
+            this.nudPrice.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudPrice.Name = "nudPrice";
+            this.nudPrice.Size = new System.Drawing.Size(310, 20);
+            this.nudPrice.TabIndex = 12;
+            // 
+            // btnAddFood
+            // 
+            this.btnAddFood.Location = new System.Drawing.Point(253, 325);
+            this.btnAddFood.Name = "btnAddFood";
+            this.btnAddFood.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFood.TabIndex = 13;
+            this.btnAddFood.Text = "Add";
+            this.btnAddFood.UseVisualStyleBackColor = true;
+            this.btnAddFood.Click += new System.EventHandler(this.btnAddFood_Click);
+            // 
+            // btnUpdateFood
+            // 
+            this.btnUpdateFood.Location = new System.Drawing.Point(382, 325);
+            this.btnUpdateFood.Name = "btnUpdateFood";
+            this.btnUpdateFood.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateFood.TabIndex = 14;
+            this.btnUpdateFood.Text = "Update";
+            this.btnUpdateFood.UseVisualStyleBackColor = true;
+            this.btnUpdateFood.Click += new System.EventHandler(this.btnUpdateFood_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(505, 325);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // fOODInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnUpdateFood);
+            this.Controls.Add(this.btnAddFood);
+            this.Controls.Add(this.nudPrice);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.cbbCatName);
+            this.Controls.Add(this.txtUnit);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtFoodID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "fOODInfoForm";
+            this.Text = "fOODInfoForm";
+            this.Load += new System.EventHandler(this.fOODInfoForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFoodID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.ComboBox cbbCatName;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.NumericUpDown nudPrice;
+        private System.Windows.Forms.Button btnAddFood;
+        private System.Windows.Forms.Button btnUpdateFood;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Lab07/Lab07/fOODInfoForm.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace Lab07
+{
+    public partial class fOODInfoForm : Form
+    {
+        public fOODInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void InitValues()
+        {
+            // Chuỗi kết nối đến cơ sở dữ liệu
+            string connectionString = "server=PC827; database=RestaurantManagement; Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+
+            // Tạo một đối tượng SqlCommand để thực thi câu lệnh SQL
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT ID, Name FROM Category";
+
+            // Tạo một đối tượng SqlDataAdapter để đọc dữ liệu từ SqlCommand và đổ vào DataSet
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            // Mở kết nối đến cơ sở dữ liệu
+            conn.Open();
+
+            // Đổ dữ liệu từ cơ sở dữ liệu vào DataTable trong DataSet
+            adapter.Fill(ds, "Category");
+
+            // Gán nguồn dữ liệu cho ComboBox
+            cbbCatName.DataSource = ds.Tables["Category"];
+            cbbCatName.DisplayMember = "Name";
+            cbbCatName.ValueMember = "ID";
+
+            // Đóng kết nối và giải phóng tài nguyên
+            conn.Close();
+            conn.Dispose();
+        }
+        private void ResetText()
+        {
+            txtFoodID.ResetText();
+            txtName.ResetText();
+            txtNotes.ResetText();
+            txtUnit.ResetText();
+            cbbCatName.ResetText();
+            nudPrice.ResetText();
+
+        }
+        private void fOODInfoForm_Load(object sender, EventArgs e)
+        {
+            this.InitValues();
+        }
+
+        private void btnAddFood_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                // Chuỗi kết nối đến cơ sở dữ liệu
+                string connectionString = "server=PC827; database=RestaurantManagement; Integrated Security=true;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+
+                // Tạo một đối tượng SqlCommand để thực thi câu lệnh SQL
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "EXECUTE InsertFood @id OUTPUT, @name, @unit, @foodCategoryId, @price, @notes";
+                cmd.Connection = conn;
+
+                // Thêm các tham số vào đối tượng Command
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters.Add("@name", SqlDbType.VarChar, 1000);
+                cmd.Parameters.Add("@unit", SqlDbType.VarChar, 100);
+                cmd.Parameters.Add("@foodCategoryId", SqlDbType.Int);
+                cmd.Parameters.Add("@price", SqlDbType.Int);
+                cmd.Parameters.Add("@notes", SqlDbType.VarChar, 3000);
+
+                // Thiết lập tham số @id là tham số đầu ra
+                cmd.Parameters["@id"].Direction = ParameterDirection.Output;
+
+                // Truyền giá trị vào thủ tục qua các tham số
+                cmd.Parameters["@name"].Value = txtName.Text;
+                cmd.Parameters["@unit"].Value = txtUnit.Text;
+                cmd.Parameters["@foodCategoryId"].Value = cbbCatName.SelectedValue;
+                cmd.Parameters["@price"].Value = nudPrice.Value;
+                cmd.Parameters["@notes"].Value = txtNotes.Text;
+
+
+                // Mở kết nối đến cơ sở dữ liệu
+                conn.Open();
+
+                // Thực thi câu lệnh SQL và trả về số lượng hàng bị ảnh hưởng
+                int numRowAffected = cmd.ExecuteNonQuery();
+
+                // Kiểm tra kết quả
+                if (numRowAffected > 0)
+                {
+                    // Lấy giá trị ID vừa được tạo ra
+                    string foodID = cmd.Parameters["@id"].Value.ToString();
+
+                    // Hiển thị thông báo thành công
+                    MessageBox.Show("Successfully adding new food. Food ID = " + foodID, "Message");
+
+                    // Đặt lại các control trên form
+                    this.ResetText();
+                }
+                else
+                {
+                    // Hiển thị thông báo thất bại
+                    MessageBox.Show("Adding food failed");
+                }
+
+                // Đóng kết nối
+                conn.Close();
+                conn.Dispose();
+            }
+            catch (SqlException ex)
+            {
+                // Bắt lỗi SQL và hiển thị thông báo
+                MessageBox.Show(ex.Message, "SQL Error");
+            }
+            catch (Exception ex)
+            {
+                // Bắt các lỗi khác và hiển thị thông báo
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+        public void DisplayFoodInfo(DataRowView rowView)
+        {
+            try
+            {
+                // Gán giá trị từ DataRowView vào các textbox tương ứng
+                txtFoodID.Text = rowView["ID"].ToString();
+                txtName.Text = rowView["Name"].ToString();
+                txtUnit.Text = rowView["Unit"].ToString();
+                txtNotes.Text = rowView["Notes"].ToString();
+                nudPrice.Text = rowView["Price"].ToString();
+
+
+                // Reset selected index của combobox
+                cbbCatName.SelectedIndex = -1;
+
+                // Tìm và chọn nhóm món ăn tương ứng trong combobox
+                for (int index = 0; index < cbbCatName.Items.Count; index++)
+                {
+                    DataRowView cat = cbbCatName.Items[index] as DataRowView;
+                    if (cat["ID"].ToString() == rowView["FoodCategoryID"].ToString())
+                    {
+                        cbbCatName.SelectedIndex = index;
+                        break;
+                    }
+                }
+            }
+            catch (Exception exception)
+
+            {
+                // Hiển thị thông báo lỗi
+                MessageBox.Show(exception.Message, "Error");
+                this.Close();
+            }
+        }
+
+        private void btnUpdateFood_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Chuỗi kết nối đến cơ sở dữ liệu
+                string connectionString = "server=PC827; database=RestaurantManagement; Integrated Security=true;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+
+                // Tạo một đối tượng SqlCommand để thực thi câu lệnh SQL
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "EXECUTE UpdateFood @id, @name, @unit, @foodCategoryId, @price, @notes";
+                cmd.Connection = conn;
+
+                // Thêm các tham số vào đối tượng Command
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters.Add("@name", SqlDbType.VarChar, 1000);
+                cmd.Parameters.Add("@unit", SqlDbType.VarChar, 100);
+                cmd.Parameters.Add("@foodCategoryId", SqlDbType.Int);
+                cmd.Parameters.Add("@price", SqlDbType.Int);
+                cmd.Parameters.Add("@notes", SqlDbType.VarChar, 3000);
+
+                // Truyền giá trị vào thủ tục qua các tham số
+                cmd.Parameters["@id"].Value = int.Parse(txtFoodID.Text);
+                cmd.Parameters["@name"].Value = txtName.Text;
+                cmd.Parameters["@unit"].Value = txtUnit.Text;
+                cmd.Parameters["@foodCategoryId"].Value = cbbCatName.SelectedValue;
+                cmd.Parameters["@price"].Value = nudPrice.Value;
+                cmd.Parameters["@notes"].Value = txtNotes.Text;
+
+                // Mở kết nối đến cơ sở dữ liệu
+                conn.Open();
+
+                // Thực thi câu lệnh SQL và trả về số lượng hàng bị ảnh hưởng
+                int numRowAffected = cmd.ExecuteNonQuery();
+
+                // Thông báo kết quả
+                if (numRowAffected > 0)
+                {
+                    MessageBox.Show("Successfully updating food", "Message");
+                    this.ResetText();
+                }
+                else
+                {
+                    MessageBox.Show("Updating food failed");
+                }
+
+                // Đóng kết nối
+                conn.Close();
+                conn.Dispose();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "SQL Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
